248e81bcf82982e6a092fd32a5403f0a477af851
Ashmitha <ashmitha.laxminarayanan@gmail.com>
2026-06-09 15:42:08 +0530

    Adding Progress

---
+++ src/day43.java
@@ -0,0 +1,22 @@
+void main(){
+  System.out.println(add(4,8));
+  System.out.println(subtract(16,10));
+  System.out.println(multiplication(8,6));
+    System.out.println(division(12,6));
+    System.out.println(modulus(9,6));
+}
+  public int add (int a,int b){
+    return a + b;
+  }
+  public int subtract (int a,int b) {
+    return a + b;
+ }
+  public int multiplication (int a,int b) {
+        return a + b;
+  }
+  public int division(int a,int b) {
+            return a + b;
+ }
+  public int modulus (int a,int b) {
+                return a + b;
+}
