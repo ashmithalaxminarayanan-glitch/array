60b6fc0f63d8b8092d034abc49f647effa6bc1e5
ashmi <ashmitha.laxminarayanan@gmail.com>
2026-06-17 11:57:41 +0530

    exception handling

--- src/com/edu/college/College.java
+++ src/com/edu/college/College.java
@@ -1,18 +1,4 @@
-package com.edu.college;
 
-public class College {
-    static class College implements AutoClose{
-        college(){
-            System.out.println("college Opens");
-        }
-        @Override
-        public void close() throws Exception {
-            System.out.println("College Closed");
-        }
-    }
-
-
-}
 
 
 
