f412ad27ed7cd5b52a929666c2721b472683f26a
ashmi <ashmitha.laxminarayanan@gmail.com>
2026-06-23 11:24:31 +0530

    Synchronized

---
+++ src/day15.java
@@ -0,0 +1,22 @@
+public class day15 {
+    static void main() throws InterruptedException {
+        Runnable chefTask = () -> {
+            String[]items = {"Dosa","Poori","Coffee"};
+            for (int i = 0; i < items.length;i++){
+                System.out.println("Cooking Item: "+ items[i]);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                }
+            };
+        Thread chefThread = new Thread(chefTask,"chef-thread");
+        chefThread.start();
+        chefThread.join();
+        }
+    }
+
+
+
+
