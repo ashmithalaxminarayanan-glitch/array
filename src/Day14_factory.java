63d81a0ecfd44b9c5ebf12384151de86d786d762
ashmi <ashmitha.laxminarayanan@gmail.com>
2026-06-23 09:50:16 +0530

    strategy

--- src/Day14_factory.java
+++ src/Day14_factory.java
@@ -7,28 +7,30 @@ public class Day14_factory {
 
         @Override
         public void send(String to, String message) {
-            System.out.println("sending Email: "+message +", to "+to);
+            System.out.println("sending Email: " + message + ", to " + to);
         }
     }
+
     class WhatsappNotification implements Notification {
 
         @Override
         public void send(String to, String message) {
-            System.out.println("sending Whatsapp: "+message +", to "+to);
+            System.out.println("sending Whatsapp: " + message + ", to " + to);
         }
     }
 
-        static Notification getNotification(Day14_factory factory, String type) {
-            return switch (type) {
-                case "Email" -> factory.new EmailNotification();
-                case "Whatsapp" -> factory.new WhatsappNotification();
-                default-> throw new IllegalArgumentException("Invalid Notification type: "+type);
-            };
-        }
-        static void main() {
+    static Notification getNotification(Day14_factory factory, String type) {
+        return switch (type) {
+            case "Email" -> factory.new EmailNotification();
+            case "Whatsapp" -> factory.new WhatsappNotification();
+            default -> throw new IllegalArgumentException("Invalid Notification type: " + type);
+        };
+    }
+
+    static void main() {
         Day14_factory factory = new Day14_factory();
 
         String type = "Whatsapp";
-        getNotification(factory,type).send("9123548946","Hello World");
-        }
+        getNotification(factory, type).send("9123548946", "Hello World");
     }
+}
